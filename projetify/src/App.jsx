cf61add7f786b167ce7c527cd9307b51bbaec148
Matheus Mannarino <mths.mannarino@gmail.com>
2026-02-23 17:01:22 -0300

    Remove unnecessary comment from App.jsx

--- projetify/src/App.jsx
+++ projetify/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-// Importe TODAS as suas páginas
+
 import HomePage from './pages/Home/HomePage';
 import CadastroUsuarioPage from './pages/CadastroUsuario/CadastroUsuarioPage';
 import LoginPage from './pages/Login/LoginPage';
